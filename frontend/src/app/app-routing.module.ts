339ddd60ce7b003772aed1da34a6c95c35a9eb2a
NourJbeli12 <nourjbeli78@gmail.com>
2024-03-30 03:10:46 +0100

    updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomePageComponent} from "./home-page/home-page.component";
-import {BoardsComponent} from "./home-page/boards/boards.component";
-import {BoardComponent} from "./home-page/boards/board/board.component";
-import { DetailsComponent } from './home-page/details/details.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ComponentComponent } from './admin/component/component.component';
@@ -13,13 +9,7 @@ const routes: Routes = [
  
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
-  {
-    path: "home", component: HomePageComponent, children: [
-      {path: "boards", component: BoardsComponent},
-      {path: "board/:id", component: BoardComponent},
-      {path: "details" , component : DetailsComponent},
-    ]
-  },
+
   {
     path: '',
     component: ComponentComponent,
