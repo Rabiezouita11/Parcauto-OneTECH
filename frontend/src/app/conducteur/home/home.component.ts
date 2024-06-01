d407d7ff08b59a8c47b816d137592494bcb14096
Rabiezouita11 <rabiezouita82@gmail.com>
2024-06-01 17:17:59 +0100

    updated

--- frontend/src/app/conducteur/home/home.component.ts
+++ frontend/src/app/conducteur/home/home.component.ts
@@ -139,10 +139,10 @@ AfterViewInit {
                         label: 'Reservations',
                         data: this.chartData, // Use the updated chartData
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'
+                            'blue', '#78ca5c', 'orange'
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'
+                            'blue', '#78ca5c', 'orange'
                         ],
                         borderWidth: 1
                     }
